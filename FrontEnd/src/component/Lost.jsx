3e2c5232485871ca5588218144f0216d4eb9fb13
Mohd Shakib <117885516+shakcodes@users.noreply.github.com>
2026-03-26 19:39:41 +0530

    Update Lost.jsx

--- FrontEnd/src/component/Lost.jsx
+++ FrontEnd/src/component/Lost.jsx
@@ -129,7 +129,7 @@ function Lost() {
       setImages([]);
       
       // Redirect after success
-      setTimeout(() => navigate('/Home'), 2000);
+      setTimeout(() => navigate('/'), 2000);
     } catch (error) {
       console.error('Error:', error);
       const errorMessage = error.response?.data?.message || 
